ae0ffca70178badea98a6791e71d3d502a3d081e
joshau kregg <kreggjoshua@gmail.com>
2023-03-07 16:49:37 -0500

    update meme state arrays

--- src/meme.jsx
+++ src/meme.jsx
@@ -5,12 +5,24 @@ import memesData from "./memesData";
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    //const [memeImage, setMemeImage] = React.useState("")
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url) 
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        })) 
     }   
 
 
@@ -38,7 +50,7 @@ export default function Meme() {
                 >make me a meme boi</button>
         
         </div>
-        <img src={memeImage} alt="doodoo" className="meme--image"/>
+        <img src={meme.randomImage} alt="doodoo" className="meme--image"/>
         
         </main> 
     )
